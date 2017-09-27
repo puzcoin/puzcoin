fef91951a60d2546a767d84e16fe61725f7ec0bb
puzcoin <akuma@mud.com.cn>
2017-09-27 11:27:32 +0800

    add header globals.hwq

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2014-2017 The Dash Core developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
+#include "globals.h"
 #include "activemasternode.h"
 #include "consensus/validation.h"
 #include "darksend.h"
